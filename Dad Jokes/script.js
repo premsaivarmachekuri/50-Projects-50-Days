0f71c219bd07f7c616547690c73767bbf28ba7d7
Premsai Vamra <premsaivarmacoding@gmail.com>
2023-07-01 22:11:36 +0530

    added script

---
+++ Dad Jokes/script.js
@@ -0,0 +1,19 @@
+const jokeEl = document.getElementById('joke')
+const jokeBtn = document.getElementById('jokeBtn')
+
+jokeBtn.addEventListener('click', generateJoke)
+
+generateJoke()
+
+function generateJoke() {
+    const config = {
+        headers: {
+            'ACCEPT': 'application/json'
+        }
+    }
+    fetch('https://icanhazdadjoke.com', config)
+    .then((response) => response.json())
+    .then((data) => {
+        jokeEl.innerHTML = data.joke
+    })
+}
