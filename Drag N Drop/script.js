be8c901b1bb5715069617b53ab34667caef63dee
PremsaiVarma <premsaivarmacoding@gmail.com>
2023-08-05 23:51:32 +0530

    added drag n drop js

---
+++ Drag N Drop/script.js
@@ -0,0 +1,24 @@
+function dragStart(){
+    console.log('drag start')
+}
+
+function dragEnd(){
+    console.log('drag end')
+}
+
+function dragOver(){
+    console.log('drag over')
+}
+
+function dragEnter(){
+    console.log('drag enter')
+}
+
+function dragLeave(){
+    console.log('drag leave')
+}
+
+function dragDrop(){
+    console.log('drag drop')
+}
+
