d3605b68e8dc980178278faf8f8ba565b195595e
PremsaiVarma <premsaivarmacoding@gmail.com>
2023-08-06 22:58:03 +0530

    updated drag n drop js

--- Drag N Drop/script.js
+++ Drag N Drop/script.js
@@ -1,24 +1,39 @@
-function dragStart(){
-    console.log('drag start')
+const fill = document.querySelector('.fill')
+const empties = document.querySelectorAll('.empty')
+
+fill.addEventListener('dragstart', dragStart)
+fill.addEventListener('dragend', dragEnd)
+
+for(const empty of empties) {
+    empty.addEventListener('dragover', dragOver)
+    empty.addEventListener('dragenter', dragEnter)
+    empty.addEventListener('dragleave', dragLeave)
+    empty.addEventListener('drop', dragDrop)
 }
 
-function dragEnd(){
-    console.log('drag end')
+function dragStart() {
+    this.className += ' hold' 
+    setTimeout(() => this.className = 'invisible', 0)
 }
 
-function dragOver(){
-    console.log('drag over')
+function dragEnd() {
+    this.className = 'fill'
 }
 
-function dragEnter(){
-    console.log('drag enter')
+function dragEnter(e){
+    e.preventDefault()
+}
+
+function dragOver(e) {
+    e.preventDefault()
+    this.className += ' hovered'
 }
 
 function dragLeave(){
-    console.log('drag leave')
+    this.className = ' empty'
 }
 
-function dragDrop(){
-    console.log('drag drop')
+function dragDrop() {
+    this.className = 'empty'
+    this.append(fill)
 }
-
